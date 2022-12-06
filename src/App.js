224475102a2517e95e043fb6e6b5dec3a0daf0c9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-05 22:43:38 -0800

    Add NotificationList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import NotificationList from "./components/NotificationList";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <NotificationList />
     </div>
   );
 }
