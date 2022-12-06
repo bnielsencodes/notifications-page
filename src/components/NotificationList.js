924790890eebb7cbccc678c69ef197da7dba74f3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-05 22:44:53 -0800

    Map data from data.js

--- src/components/NotificationList.js
+++ src/components/NotificationList.js
@@ -1,5 +1,10 @@
 import Notification from "./Notification";
+import data from "../data";
 
 export default function NotificationList() {
+  const notifications = data.map((item) => {
+    return <Notification key={item.id} item={item} />;
+  });
+
   return <div className="NotificationList">{notifications}</div>;
 }
