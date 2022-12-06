65ef0379f6051c30878a7e4e1de11ec51ecbd2a6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-05 22:42:12 -0800

    Add NotificationCount component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import NotificationCount from "./NotificationCount";
+
 export default function Header() {
   return (
     <div className="Header">
+      <NotificationCount />
     </div>
   );
 }
---
+++ src/components/NotificationCount.js
@@ -0,0 +1,10 @@
+export default function NotificationCount() {
+  return (
+    <div className="NotificationCount">
+      <h1>Notifications</h1>
+      <div>
+        <span>3</span>
+      </div>
+    </div>
+  );
+}
