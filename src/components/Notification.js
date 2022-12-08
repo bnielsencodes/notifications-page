fcdd90c6f78efb9aabbd0eeb750ab5e80127563b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-07 23:40:49 -0800

    Update Notification.js

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,7 +1,7 @@
 export default function Notification(props) {
   return (
     <div className="Notification">
-      <div>
+      <div className="Notification--main">
         <img
           src={require(`../assets/images/${props.item.img}`)}
           alt="user avatar"
@@ -9,16 +9,21 @@ export default function Notification(props) {
         <div>
           <div>
             <p>
-              <span>{props.item.name}</span>
               <div>
-                <span>{props.item.action}</span>
-                <span>{props.item.link}</span>
+                <span className="name">{props.item.name}</span>
+                <span className="action">{props.item.action}</span>
+                <span className="link primary-link">
+                  {props.item.primaryLink}
+                </span>
+                <span className="link secondary-link">
+                  {props.item.secondaryLink}
+                </span>
                 {/* <span></span> */}
               </div>
             </p>
             <span>{props.item.badge}</span>
           </div>
-          <p>{props.item.time}</p>
+          <p className="time">{props.item.time}</p>
         </div>
       </div>
       <div>{props.item.message}</div>
