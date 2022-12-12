adb8e78b7e6972813e473f7fde7fa52b3e1e692e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-11 23:53:54 -0800

    Add some state for unread badges

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Notification from "./components/Notification";
 import "./App.css";
 
 export default function App() {
+  // read notification state
+  const [read, setRead] = React.useState(false);
+  console.log(read);
+
+  // styling for unread notification badges
+  const badgeStyles = {
+    display: read ? "none" : "inline-block",
+    backgroundColor: read ? "transparent" : "#f65552",
+  };
+
   // map through data.js to create Notification components
   const notifications = data.map((item) => {
     return (
