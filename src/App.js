7762711f29ec1285a7f163547d0345ee4f7c47cd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-11 23:53:28 -0800

    Map through data.js to render Notification components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import Notification from "./components/Notification";
 import "./App.css";
 
 export default function App() {
+  // map through data.js to create Notification components
+  const notifications = data.map((item) => {
+    return (
+      <Notification
+        key={item.id}
+        item={item}
+        readValue={item.read}
+        badgestyles={badgeStyles}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Header handleClick={markAllRead} />
