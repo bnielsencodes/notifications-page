535c91f119a0eaf8d7a867930e7773fc3ea9c58d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-11 23:52:29 -0800

    Update Notification.js

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,32 +1,25 @@
 export default function Notification(props) {
   return (
-    <div className="Notification">
-      <div className="Notification--main">
-        <img
-          src={require(`../assets/images/${props.item.img}`)}
-          alt="user avatar"
-        />
+    <div className="notification">
+      <img
+        src={require(`../assets/images/${props.item.img}`)}
+        alt="user avatar"
+      />
+      <div>
         <div>
-          <div>
-            <p>
-              <div>
-                <span className="name">{props.item.name}</span>
-                <span className="action">{props.item.action}</span>
-                <span className="link primary-link">
-                  {props.item.primaryLink}
-                </span>
-                <span className="link secondary-link">
-                  {props.item.secondaryLink}
-                </span>
-                {/* <span></span> */}
-              </div>
-            </p>
-            <span>{props.item.badge}</span>
-          </div>
-          <p className="time">{props.item.time}</p>
+          <span className="name">{props.item.name}</span>
+          <span className="action">{props.item.action}</span>
+          <span className="link primary-link">{props.item.primaryLink}</span>
+          <span className="link secondary-link">
+            {props.item.secondaryLink}
+          </span>
+          <span className="badge" style={props.badgestyles}></span>
+        </div>
+        <p className="time">{props.item.time}</p>
+        <div id={props.item.messageId} className="message">
+          {props.item.message}
         </div>
       </div>
-      <div>{props.item.message}</div>
     </div>
   );
 }
