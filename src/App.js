38fd55e6ad2238ce6c6f8a617026ede37323fc41
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-12-31 14:01:53 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import Header from "./components/Header";
 import NotificationList from "./components/NotificationList";
 import Notification from "./components/Notification";
-import "./App.css";
 
 export default function App() {
-  // read notification state
-  const [read, setRead] = React.useState(false);
-  console.log(read);
+  const [notification, setNotification] = useState(data);
 
-  // styling for unread notification badges
   const badgeStyles = {
     display: read ? "none" : "inline-block",
     backgroundColor: read ? "transparent" : "#f65552",
